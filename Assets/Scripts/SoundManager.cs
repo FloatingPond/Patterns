aaa659076c0f91ec0680a7cde6e63fc404793806
MrResistance <Paul1208Gush@Gmail.com>
2021-04-14 23:07:30 +0100

    Started sound manager 🎧

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource Music, SFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        SFX.clip = clip;
+        SFX.Play();
+    }
+    public void PlayMusic(AudioClip track)
+    {
+        Music.clip = track;
+        Music.Play();
+    }
+}

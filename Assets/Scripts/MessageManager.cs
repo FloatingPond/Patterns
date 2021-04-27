ad210942b61e8df8d41d15a224a30b680dc6494f
floatPond <32678960+floatPond@users.noreply.github.com>
2021-04-27 23:12:14 +0100

    SC - Two new scripts for message boxes

---
+++ Assets/Scripts/MessageManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    //This script is for managing the message box(s)
+
+    public GameObject MainMessageBox;
+    public GameObject FallenStreakMessageBox;
+
+
+    public void CloseMessage()
+    {
+        //Hides both
+    }
+
+    public void DisplayFirstWelcomeMessage()
+    {
+
+        //Set title to 'Welcome'
+        //Set text to 'Something'
+    }
+
+
+}
